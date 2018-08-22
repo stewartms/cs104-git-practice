430a2fbd9fa91f322301ef67a8c7ece1d7f5731e
Stewart Smith <stewarms@usc.edu>
2018-08-22 10:19:36 -0700

    My first HelloWorld using git

---
+++ HelloWorld.cpp
@@ -0,0 +1,8 @@
+#include iostream
+
+int main()
+{
+	std::cout << "HenloWorld" << std::endl;
+	std::cout << "Hello You Stinky World" << std::endl;
+	std::cout << "Go run my code, ugly" << std::endl;
+}
